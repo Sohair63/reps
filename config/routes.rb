758da5a9ad4bcfd18f25520aa23fa7b98a433166
Sohair <sohair1991@gmail.com>
2015-11-15 23:35:55 +0500

    Active Admin: Cofigured + Added property model + added admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Reps::Application.routes.draw do
 
   root to: "properties#index"
-
   resources :properties
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
 end
