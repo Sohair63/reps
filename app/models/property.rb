cb4f6fac1880b969cd58d0fc49bf77c408a5dff4
Sohair <sohair1991@gmail.com>
2015-11-16 10:29:43 +0500

    Property: Added model level validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,18 @@ class Property < ActiveRecord::Base
   paginates_per 10
 
   attr_accessible :address, :area, :overview, :price, :status, :title, :property_type
+
+  validates :address,       presence: true
+  validates :area,          presence: true
+  validates :price,         presence: true
+  validates :title,         presence: true
+
+  validates :status,        presence: true,
+                            inclusion: { in: %w(sale rent wanted),
+                            message: "%{value} is not a valid status" },
+                            allow_nil: false
+  validates :property_type, presence: true,
+                            inclusion: { in: %w(homes plot commercial),
+                            message: "%{value} is not a valid property type" },
+                            allow_nil: false
 end
